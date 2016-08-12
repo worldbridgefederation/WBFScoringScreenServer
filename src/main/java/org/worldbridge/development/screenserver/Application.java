a4ad86ae5834291d33615d538bb0c0a335a08288
Hugo Trippaers <hugo@trippaers.nl>
2016-08-12 17:45:21 +0200

    Cleanup and consistency

--- src/main/java/org/worldbridge/development/screenserver/Application.java
+++ src/main/java/org/worldbridge/development/screenserver/Application.java
@@ -1,15 +1,6 @@
 package org.worldbridge.development.screenserver;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.worldbridge.development.screenserver.hsqldb.HyperSqlDbServer;
-import org.worldbridge.development.screenserver.rest.CrashService;
-import org.worldbridge.development.screenserver.rest.NotificationService;
-import org.worldbridge.development.screenserver.rest.ScreenGroupService;
-import org.worldbridge.development.screenserver.rest.StatusService;
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.endpoint.Server;
@@ -19,9 +10,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.worldbridge.development.screenserver.rest.CrashService;
+import org.worldbridge.development.screenserver.rest.NotificationService;
+import org.worldbridge.development.screenserver.rest.ScreenGroupService;
+import org.worldbridge.development.screenserver.rest.StatusService;
 
-import javax.sql.DataSource;
 import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class Application {
@@ -40,7 +35,6 @@ public class Application {
         return new SpringBus();
     }
 
-
     @Bean
     public StatusService statusService() {
         return new StatusService();
@@ -57,14 +51,20 @@ public class Application {
     }
 
     @Bean
+    public CrashService crashService() {
+        return new CrashService();
+    }
+
+    @Bean
     public Server rsServer() {
         JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
         endpoint.setBus(springBus());
         endpoint.setAddress("/");
 
         endpoint.setServiceBeans(Arrays.asList(statusService(), screenGroupService(), notificationService(),
-                new CrashService()));
-        endpoint.setProviders(Arrays.asList(new JacksonJsonProvider()));
+                crashService()));
+        endpoint.setProviders(Collections.singletonList(new JacksonJsonProvider()));
+
         return endpoint.create();
     }
 }
